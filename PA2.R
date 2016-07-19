f61ebad1396f5c48c66b8e348b19fa08a565cf7d
Chuk Yong <chuk_yong@hotmail.com>
2016-07-19 17:40:53 +0800

    ver 2

--- PA2.R
+++ PA2.R
@@ -3,7 +3,9 @@ Data <- read.csv("repdata-data-StormData.csv")
 
 library(dplyr)
 
-FataData <- Data %>% group_by(EVTYPE) %>% summarise(FATAL=sum(FATALITIES)+sum(INJURIES)) %>% arrange(desc(FATAL))
+FataData <- Data %>% group_by(EVTYPE) %>% 
+  summarise(FATAL=sum(FATALITIES)+sum(INJURIES)) %>% 
+  arrange(desc(FATAL))
 FataDataClean <- FataData[1:20,]
 
 # Order the dataframe with the right factor level.  If not, ggplot will order them alphabetically
@@ -15,6 +17,30 @@ plot <- ggplot(FataDataClean, aes(x=FataDataClean$EVTYPE, y=FataDataClean$FATAL)
   geom_bar(stat = "identity") + 
   # Flip the x-y so that the "Type" is easier to read
   coord_flip() +
-  xlab("Type of Event") + ylab("Number of Fatality") + ggtitle("Top 20 Fatal Weather Events in the United States")
+  xlab("") + ylab("Number of Fatality") + ggtitle("Top 20 Fatal Weather Events in the United States")
+plot
+
+# Calculate economic damages: total of property and crop damages
+
+# create a data frame of Economic Damage Data.Where PROPDMG and CROPDMG both has values, convert "K, M, B" to appropiate amount, total and rank them
+EconData <- Data %>%
+  filter(PROPDMG>0 | Data$CROPDMG>0) %>%
+  select(EVTYPE,PROPDMG,PROPDMGEXP,CROPDMG,CROPDMGEXP) %>%
+  mutate(PROPDMG = PROPDMG*(ifelse(PROPDMGEXP == "K",1000, ifelse(PROPDMGEXP == "M",1000000,ifelse(PROPDMGEXP == "B",1000000000,1))))) %>%
+  mutate(CROPDMG = CROPDMG*(ifelse(CROPDMGEXP == "K",1000, ifelse(CROPDMGEXP == "M",1000000,ifelse(CROPDMGEXP == "B",1000000000,1))))) %>%
+  mutate(TOTDMG = PROPDMG+CROPDMG)
+
+EconDataSum <- EconData %>% group_by(EVTYPE) %>% 
+  summarise(TOTECONDMG = sum(TOTDMG)) %>%
+  arrange(desc(TOTECONDMG)) %>%
+  slice(1:20)
+
+EconDataSum$EVTYPE <- factor(EconDataSum$EVTYPE, levels = EconDataSum$EVTYPE[order(EconDataSum$TOTECONDMG)])
+
+plot <- ggplot(EconDataSum, aes(x=EconDataSum$EVTYPE, y=EconDataSum$TOTECONDMG)) +
+  geom_bar(stat = "identity") +
+  # Flip the x-y so that the "Type" is easier to read
+  coord_flip() + 
+  xlab("") + ylab("Economic Cost ") + ggtitle("Top 20 Most Costly Weather Events (USA)")
 plot
 
